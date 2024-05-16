0c62e410d5653a87f8ece3a9bafb3cb582cc69c1
Volodymyr <110925314+woland31252@users.noreply.github.com>
2024-05-17 01:45:17 +0300

    fix ContactPage

--- src/pages/ContactsPage/ContactsPage.jsx
+++ src/pages/ContactsPage/ContactsPage.jsx
@@ -1,5 +1,37 @@
+// export default function ContactsPage() {
+//     return (
+//         <p>Contacts Page</p>
+//     )
+// }
+
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchContacts } from '../../redux/contacts/operations.js';
+// import { selectLoading, selectError } from '../../redux/contactsSlice.js';
+import { selectLoading, selectError } from '../../redux/contacts/selectors.js';
+import FetchError from '../../components/FetchError/FetchError.jsx';
+import ContactForm from '../../components/ContactForm/ContactForm.jsx';
+import ContactList from '../../components/ContactList/ContactList.jsx';
+import SearchBox from '../../components/SearchBox/SearchBox.jsx';
+import Loader from '../../components/Loader/Loader.jsx';
+import css from '../ContactsPage/ContactsPage.module.css';
+
 export default function ContactsPage() {
-    return (
-        <p>Contacts Page</p>
-    )
+  const dispatch = useDispatch();
+  const isLoading = useSelector(selectLoading);
+  const isError = useSelector(selectError);
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
+  return (
+    <div className={css.container}>
+      <h1 className={css.phoneTitle}>Phonebook</h1>
+      <ContactForm />
+      {isLoading && <Loader/>}
+      {isError ? <FetchError /> : <SearchBox />}
+      <ContactList/>
+    </div>
+  )
 }
